72c050d72c1b899203893778fd2639e3832b941d
hoxis <liuhao1960@gmail.com>
2015-05-12 18:59:14 +0800

    增加了需要验证码时验证码的显示，点击图片可以刷新验证码，优化提示语

--- app/src/main/java/com/liuhao/douban/ui/LoginActivity.java
+++ app/src/main/java/com/liuhao/douban/ui/LoginActivity.java
@@ -2,9 +2,11 @@ package com.liuhao.douban.ui;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +15,7 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.liuhao.douban.R;
+import com.liuhao.douban.util.ImageUtil;
 import com.liuhao.douban.util.NetWorkUtil;
 
 
@@ -24,6 +27,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     private static final int NEED_CAPTCHA = 10;
     private static final int NOT_NEED_CAPTCHA = 11;
     private static final int GET_CAPTCHA_ERROR = 12;
+    private static final String TAG = "LoginActivity";
     private EditText mEmailEditText;
     private EditText mPwdEditText;
     private LinearLayout mCaptchaLinearLayout;
@@ -32,6 +36,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     private Button mLoginButton;
     private Button mBackButton;
     private ProgressDialog pd;
+    private Bitmap bitmap;
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -41,11 +46,12 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             switch (msg.what) {
                 case NEED_CAPTCHA:
                     mCaptchaLinearLayout.setVisibility(View.VISIBLE);
+                    mCaptchaImageView.setImageBitmap(bitmap);
                     break;
                 case NOT_NEED_CAPTCHA:
                     break;
                 case GET_CAPTCHA_ERROR:
-                    Toast.makeText(getApplicationContext(), "查询验证码失败", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "加载失败，请检查网络设置", Toast.LENGTH_SHORT).show();
                     break;
             }
         }
@@ -69,28 +75,39 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         mEmailEditText = (EditText) this.findViewById(R.id.editText_email);
         mPwdEditText = (EditText) this.findViewById(R.id.editText_password);
         mCaptchaEditTextValue = (EditText) this.findViewById(R.id.editText_captchaValue);
+        mCaptchaImageView = (ImageView) this.findViewById(R.id.imageView_captcha);
         mCaptchaLinearLayout = (LinearLayout) this.findViewById(R.id.ll_captcha);
         mLoginButton = (Button) this.findViewById(R.id.btn_login);
         mBackButton = (Button) this.findViewById(R.id.btn_back);
+        getCaptcha();
 
+
+    }
+
+    private void getCaptcha() {
         //查询验证码操作属于联网查询，耗时较长，因此采用一个ProgressDialog进行缓冲
         pd = new ProgressDialog(this);
-        pd.setMessage("正在查询是否需要验证码");
+        pd.setMessage("玩命加载中...");
+        pd.setCancelable(false);
         pd.show();
 
         //判断是否需要验证码，然后设置mCaptchaLinearLayout的可见属性
         new Thread() {
             @Override
             public void run() {
-                Message msg = null;
+                Message msg = new Message();
                 try {
-                    boolean result = NetWorkUtil.isNeedCaptcha(getApplicationContext());
+                    String result = NetWorkUtil.isNeedCaptcha(getApplicationContext());
+                    Log.i(TAG, result);
                     msg = new Message();
-                    if (result) {
-                        msg.what = NEED_CAPTCHA;
+                    if (result == null) {
+                        msg.what = NOT_NEED_CAPTCHA;
                         handler.sendMessage(msg);
                     } else {
-                        msg.what = NOT_NEED_CAPTCHA;
+                        msg.what = NEED_CAPTCHA;
+//                        String imgUrl = getResources().getString(R.string.imgUrl) + result + "&amp;size=s";
+//                        bitmap = ImageUtil.getImageByURL(imgUrl);
+                        bitmap = ImageUtil.getImageByURL(result);
                         handler.sendMessage(msg);
                     }
                 } catch (Exception e) {
@@ -108,6 +125,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     private void setListener() {
         mLoginButton.setOnClickListener(this);
         mBackButton.setOnClickListener(this);
+        mCaptchaImageView.setOnClickListener(this);
     }
 
     @Override
@@ -118,6 +136,9 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.btn_back:
                 finish();
+                break;
+            case R.id.imageView_captcha:
+                getCaptcha();
                 break;
         }
     }
